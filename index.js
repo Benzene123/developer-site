7cdded32bfe5d114da9ebd5e1c8f094ef2c2d161
Benzene <oluuwolevictor70@gmail.com>
2023-11-25 12:29:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+document.querySelector(".menu").addEventListener("click", function () {
+    document.querySelector(".slideshow").style.display ="block";
+})
+
+document.querySelector(".on").addEventListener("click", function () {
+    document.querySelector(".slideshow").style.display ="none";
+})
+
+
+
